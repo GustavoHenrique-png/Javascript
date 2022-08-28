8a9787267cd14112c65825aaf617047c901409b1
Gustavo Henrique <ymooncake@gmail.com>
2022-08-28 10:09:07 -0300

    Update funcionalidades.js

--- public/aula01/scripts/funcionalidades.js
+++ public/aula01/scripts/funcionalidades.js
@@ -1,2 +1,15 @@
 let vaquinha = `vaca loka`
 console.log(vaquinha)
+
+function criaAluno(nome, primeiraNota, segundaNota) {
+  let aluno = {}
+
+  aluno.nome = nome
+  aluno.primeiraNota = primeiraNota
+  aluno.segundaNota = segundaNota
+
+  return aluno
+}
+
+let aluno1 = criaAluno('jorge',1,3)
+console.log(aluno1)
